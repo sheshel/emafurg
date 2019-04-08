5cf3d293d09a6fae626cc87d988742905c49c31c
Athila Zuma <athzuma@gmail.com>
2019-04-07 22:13:10 -0300

    Instruções de configuração

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+//Iniciar um projeto nodejs 	$npm init -y
+//Instalar o express			$npm add express
+//Instalar o body-parser		$npm add body-parser
+
 const express = require('express'); 					//Biblioteca do nodeJS
 const bodyParser = require('body-parser'); 				//Biblioteca do nodeJS
 require('dotenv').config(); 							// P/ Trabalhar com variaveis de ambiente
@@ -6,7 +10,6 @@ const app = express();									//Cria o app
 
 app.use(bodyParser.json());								//Configura para usar json
 app.use(bodyParser.urlencoded({ extend: false }));		//Configura para usar urlencode
-
 
 const PORT = process.env.PORT || 8089;					//Usar a porta 8089 em localhost ou a padrao definida no server
 app.listen(PORT,() => {
